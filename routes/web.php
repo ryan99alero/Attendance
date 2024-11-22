8a4991b24ee21f530344eb7df0f16bde17c837f1
Ryan99alero <ryan@randgraphics.com>
2024-11-22 01:59:07 -0600

    Push latest updates.

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,8 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+Route::group(['prefix' => 'admin'], function () {
+    Voyager::routes();
+});
