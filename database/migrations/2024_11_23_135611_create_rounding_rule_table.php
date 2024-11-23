514935eade6caf71638ddb54e96fda386946d5f3
Ryan99alero <ryan@randgraphics.com>
2024-11-23 13:57:09 -0600

    Remove old Migrations and add new ones.

---
+++ database/migrations/2024_11_23_135611_create_rounding_rule_table.php
@@ -0,0 +1,27 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('rounding_rule', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('rounding_rule');
+    }
+};
