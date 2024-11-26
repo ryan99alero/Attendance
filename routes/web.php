7b63d05239f7bdbe856b3e10e4d5b8101f4e7302
Ryan99alero <ryan@randgraphics.com>
2024-11-25 21:05:36 -0600

    cleaned web.php removed reference to Voyager.

--- routes/web.php
+++ routes/web.php
@@ -5,8 +5,3 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
-
-
-Route::group(['prefix' => 'admin'], function () {
-    Voyager::routes();
-});
