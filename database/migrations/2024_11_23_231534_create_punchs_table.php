2b3250127857bcc3e2aee017c18000d0fa8c0fc3
Ryan99alero <ryan@randgraphics.com>
2024-12-03 22:25:43 -0600

    Updated Punches Table to have field to hold make a relationship to Pay Periods

--- database/migrations/2024_11_23_231534_create_punchs_table.php
+++ database/migrations/2024_11_23_231534_create_punchs_table.php
@@ -14,6 +14,7 @@ return new class extends Migration
             $table->unsignedBigInteger('employee_id')->nullable()->comment('Foreign key to Employees');
             $table->unsignedBigInteger('device_id')->nullable()->comment('Foreign key to Devices');
             $table->unsignedBigInteger('punch_type_id')->nullable()->comment('Foreign key to Punch Types');
+            $table->unsignedBigInteger('pay_period_id')->nullable()->comment('Foreign key to Pay Periods'); // New field
             $table->timestamp('time_in')->comment('Actual punch-in time');
             $table->timestamp('time_out')->nullable()->comment('Actual punch-out time');
             $table->boolean('is_altered')->default(false)->comment('Indicates if the punch was altered post-recording');
@@ -25,6 +26,7 @@ return new class extends Migration
             $table->foreign('employee_id')->references('id')->on('employees')->onDelete('cascade');
             $table->foreign('device_id')->references('id')->on('devices')->onDelete('set null');
             $table->foreign('punch_type_id')->references('id')->on('punch_types')->onDelete('set null');
+            $table->foreign('pay_period_id')->references('id')->on('pay_periods')->onDelete('set null'); // New constraint
             $table->foreign('created_by')->references('id')->on('users')->onDelete('set null');
             $table->foreign('updated_by')->references('id')->on('users')->onDelete('set null');
         });
